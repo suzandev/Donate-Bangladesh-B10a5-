3fd1ccd39b89280f3d1d871530cbdb291899c015
suzandev <dev.suzanchandra@gmail.com>
2024-09-22 22:00:50 +0600

    Donate Bangladesh project setup and header section work

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(180, 244, 97)",
+        secondary: "rgb(249, 247, 243)",
+        "dark-primary": "rgb(17, 17, 17)",
+        "dark-secondary": "rgba(17, 17, 17, 0.7)",
+      },
+
+      fontFamily: {
+        lexend: "Lexend", // Adds a new `font-display` class
+      },
+    },
+  },
+};
